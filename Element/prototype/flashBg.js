94682609b06b064108917755b5cc42efe5b9d72b
visco <visco@874840ac-8b26-453e-a097-40cf495e22eb>
2011-02-04 16:21:43 +0000

    fixed problem with sb.notifer unique message checker that prevented it from displaying messages ending in punctuation or numbers

--- Element/prototype/flashBg.js
+++ Element/prototype/flashBg.js
@@ -6,7 +6,7 @@ myElement.flashBg('red');
 */
 Element.prototype.flashBg = function(color, timeout){
 	timeout = timeout || 1000;
-
+	
 	this.style.backgroundColor = color;
 	var t = this;
 	window.setTimeout(function(){
