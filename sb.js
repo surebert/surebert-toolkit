ce0f8f815077fb16f99009a38091a415ff724137
“Visco, <paul.visco@roswellpark.org>
2014-02-07 19:01:46 -0500

    *removed js version map as it is no longer needed

--- sb.js
+++ sb.js
@@ -1413,14 +1413,6 @@ if (typeof console === 'undefined') {
 		} else if (document.all || (agent.match(ie11p) && !agent.match(firefox))){
 			
 			this.agent = 'ie';
-			var verMap = {
-                            "5.5": "5.5",
-                            "5.6": "6",
-                            "5.7": "7",
-                            "5.8": "8",
-                            "9": "9",
-                            "10": "10"
-                        };
                         var ver = new Function("/*@cc_on return @_jscript_version; @*/")();
                         
                         if (ver !== undefined) {
